273a65e66e23f2c7525fad6cf798b3151aa8b733
Ernoff <ernest.offiong@gmail.com>
2017-10-05 16:27:06 +0100

    hooks being set up

--- public/app.js
+++ public/app.js
@@ -1,18 +1,49 @@
 const socket = io();
-const client = feather();
-cllient.configure(feathers.hooks());
+const client = feathers();
+client.configure(feathers.hooks());
 
-client.configure(featehrs.socketio(socket));
+client.configure(feathers.socketio(socket));
 
 const messages = client.service('messages');
 
-messages.on('created', message =>
-    alert(`New Message from ${message.name}: ${message.text}`)
-);
+// client.configure(feathers.authentication({
+//     storage: window.localStorage
+// }));
 
-messages.create({
-    name: 'Test User',
-    text: 'Hello World!'
+// client.authenticate({
+//     strategy: 'local',
+
+// })
+
+function addMessage(message) {
+    const chat = document.querySelector('.chat');
+
+    chat.insertAdjacentHTML('beforeend', `<div>
+        <img src="https://placeimg.com/64/64/any" alt="${message.name}" class="avatar">
+        <div class="message-wrapper">
+            <p class="message-wrapper">
+                <scan class="username font=600">${message.name}</scan>
+            </p>
+            <p class="message-content font-300">${message.text}</p>
+        </div>
+        </div>`);
+
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+}
+
+messages.find().then(page => page.data.forEach(addMessage));
+
+messages.on('created', addMessage);
+
+document.getElementById('send-message').addEventListener('submit', function(ev) {
+    const nameInput = document.querySelector('[name="name"]');
+    const textInput = document.querySelector('[name="text"]');
+
+    client.service('messages').create({
+        name: nameInput.value,
+        text: textInput.value
+    }).then(() => {
+        textInput.value = '';
+    });
+    ev.preventDefault();
 });
-
-messages.find().then(page => console.log('Current Messages are', page));
--- src/services/messages/messages.hooks.js
+++ src/services/messages/messages.hooks.js
@@ -1,33 +1,48 @@
+const { authenticate } = require('feathers-authentication').hooks;
+const { populate } = require('feathers-hooks-common');
+
+const processMessage = require('../../hooks/process-message');
 
 
 module.exports = {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [authenticate('jwt')],
+        find: [],
+        get: [],
+        create: [processMessage()],
+        update: [processMessage()],
+        patch: [processMessage()],
+        remove: []
+    },
 
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [
+            populate({
+                schema: {
+                    include: [{
+                        service: 'users',
+                        nameAs: 'user',
+                        parentField: 'userId',
+                        childField: '_id'
+                    }]
+                }
+            })
+        ],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
 };
